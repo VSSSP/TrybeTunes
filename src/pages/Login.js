367d6cbf87bc5eec3ac721bf58a550a9b4122d0c
VSSSP <victorssspaula@live.com>
2021-09-14 16:19:06 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Login extends Component {
   constructor() {
@@ -7,20 +9,52 @@ export default class Login extends Component {
 
     this.state = {
       disabled: true,
+      criarUsuario: {},
+      load: false,
+      redirect: false,
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange = (event) => {
+  handleChange = ({ target }) => {
+    const { value } = target;
     const MIN_LENGTH = 3;
-    if (event.value.length > MIN_LENGTH) {
+    if (value.length >= MIN_LENGTH) {
       this.setState({
         disabled: false,
+        criarUsuario: {
+          name: value,
+        },
+      });
+    } else {
+      this.setState({
+        disabled: true,
       });
     }
   };
 
+  async handleClick() {
+    const { criarUsuario } = this.state;
+    this.setState({
+      load: true,
+      redirect: false,
+    });
+    await createUser(criarUsuario);
+    this.setState({
+      load: false,
+      redirect: true,
+    });
+  }
+
   render() {
-    const { disabled } = this.state;
+    const { disabled, redirect, load } = this.state;
+    if (load) {
+      return <Loading />;
+    }
+    if (redirect) {
+      return <Redirect to="/search" />;
+    }
+
     return (
       <div data-testid="page-login">
         <form>
@@ -31,11 +65,10 @@ export default class Login extends Component {
               onChange={ this.handleChange }
             />
             <button
-              createUser={ createUser }
               data-testid="login-submit-button"
-              type="submit"
-              name={ disabled }
-              onChange={ this.handleChange }
+              type="button"
+              disabled={ disabled }
+              onClick={ this.handleClick }
             >
               Entrar
             </button>
