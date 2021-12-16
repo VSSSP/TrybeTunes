55ec2e4a7c2a67a3a10df77692f21cd841fa9bcf
VSSSP <victorssspaula@live.com>
2021-12-16 11:39:55 -0300

    actions

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -61,6 +61,7 @@ export default class Login extends Component {
                onChange={ this.handleChange }
              />
             </div>
+            <br />
              <button
                data-testid="login-submit-button"
                type="button"
